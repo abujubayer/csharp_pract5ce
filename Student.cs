f83dc3b3097239df5359be680eceec0103da465e
abu jubayer <59165487+abujubayer@users.noreply.github.com>
2023-06-26 14:01:46 +0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Course
+{
+    internal class Student
+    {
+        public List <Course> EnrolledCourses = new List <Course> ();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List <string> EmailCollection = new List<string>();
+
+
+        public double GetCGPA()
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
